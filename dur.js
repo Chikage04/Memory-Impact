c03fb3cbfa8ff70704da06c81e26a8f1920c1887
Chikage04 <104400056+Chikage04@users.noreply.github.com>
2023-01-25 01:05:01 +0100

    Add files via upload

--- dur.js
+++ dur.js
@@ -42,6 +42,38 @@ const array = [
 
 ];
 window.addEventListener("load", (event) => {
+    document.querySelectorAll(".glow-button").forEach((button) => {
+        const gradientElem = document.createElement("div");
+        gradientElem.classList.add("gradient");
+    
+        button.appendChild(gradientElem);
+    
+        button.addEventListener("pointermove", (e) => {
+            const rect = button.getBoundingClientRect();
+    
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top;
+    
+            gsap.to(button, {
+                "--pointer-x": `${x}px`,
+                "--pointer-y": `${y}px`,
+                duration: 0.6,
+            });
+    
+            gsap.to(button, {
+                "--button-glow": chroma
+                    .mix(
+                        getComputedStyle(button)
+                            .getPropertyValue("--button-glow-start")
+                            .trim(),
+                        getComputedStyle(button).getPropertyValue("--button-glow-end").trim(),
+                        x / rect.width
+                    )
+                    .hex(),
+                duration: 0.2,
+            });
+        });
+    });
     // Mélange les éléments de l'array de manière aléatoire
     function shuffle(arr) {
         for (let i = arr.length - 1; i > 0; i--) {
